b3dd19dd0a2b7fd78b97d515a64efdceb5d78ec3
TiDurak <ivanplisko9@gmail.com>
2026-08-30 00:23:33 +0200

    new cards

---
+++ services/GovnoedWeb/internal/data/cards.go
@@ -0,0 +1,95 @@
+package data
+
+type Card struct {
+	Name  string
+	Title string
+	Price int
+}
+
+var Cards = []Card{
+	{
+		Name:  "basic",
+		Title: "Базовая Бомжекарта",
+		Price: 0,
+	},
+	{
+		Name:  "basic1",
+		Title: "Говносиняя Бомжекарта",
+		Price: 4000,
+	},
+	{
+		Name:  "basic2",
+		Title: "Лесбиянкозелёная Бомжекарта",
+		Price: 5000,
+	},
+	{
+		Name:  "dolbaeb",
+		Title: "Долбаёб",
+		Price: 35000,
+	},
+	{
+		Name:  "ebat_ti_lox",
+		Title: "Ебать ты лох",
+		Price: 35000,
+	},
+	{
+		Name:  "gay",
+		Title: "Гей мод",
+		Price: 10000,
+	},
+	{
+		Name:  "lox",
+		Title: "Лох",
+		Price: 50000,
+	},
+	{
+		Name:  "metalhead",
+		Title: "Я у мамы митолхед ЫЫЫЫ",
+		Price: 75000,
+	},
+	{
+		Name:  "neon",
+		Title: "Неоновая ебатория",
+		Price: 150000,
+	},
+	{
+		Name:  "penek",
+		Title: "На пенёк сел - должен был косарь отдать",
+		Price: 14880,
+	},
+	{
+		Name:  "ricardo",
+		Title: "Рикардо милос",
+		Price: 50000,
+	},
+	{
+		Name:  "dismember",
+		Title: "А я красавчик, слушаю дэт метал",
+		Price: 150000,
+	},
+	{
+		Name:  "trve_kvlt",
+		Title: "Ьы а я слушаю бурзум и имортыл",
+		Price: 250000,
+	},
+	{
+		Name:  "savinewood",
+		Title: "Вайнвуд",
+		Price: 25000,
+	},
+	{
+		Name:  "sasunshine",
+		Title: "Карта \"ностальгия\"",
+		Price: 85000,
+	},
+	{
+		Name:  "vibori",
+		Title: "Выборы выборы - кандидаты пидоры",
+		Price: 65000,
+	},
+	{
+		Name:  "lukashenko",
+		Title: "Сейчас я вам покажу, откуда на а плент готовилось нападение",
+		Price: 65000,
+	},
+}
