5c0dca4f09a698652a504fa38349f0cb7506439a
TiDurak <ivanplisko9@gmail.com>
2026-08-30 13:28:35 +0200

    Open Graph meta tags

--- services/GovnoedWeb/components/layout/head_templ.go
+++ services/GovnoedWeb/components/layout/head_templ.go
@@ -29,33 +29,46 @@ func Head(title, description string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"theme-color\" content=\"#161618\"><meta name=\"description\" content=\"Первый русскоязычный сайт для программистов-говноедов\"><meta name=\"keywords\" content=\"говно, говноед, govno, govnoed, gdisclaimer, gamerdisclaimer, debil, debilbot, \r\n        дебил, дебилбот\"><meta name=\"author\" content=\"gdisclaimer\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"manifest\" href=\"/static/favicons/manifest.webmanifest\"><link rel=\"icon\" type=\"image/x-icon\" href=\"/static/favicons/favicon.ico\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-16x16.png\" sizes=\"16x16\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-32x32.png\" sizes=\"32x32\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-192x192.png\" sizes=\"192x192\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-512x512.png\" sizes=\"512x512\"><link rel=\"apple-touch-icon\" href=\"/static/favicons/apple-touch-icon.png\"><meta property=\"og:title\" content=\"{ title } - govnoed.de\"><meta property=\"og:description\" content=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><meta charset=\"UTF-8\"><meta name=\"theme-color\" content=\"#161618\"><meta name=\"description\" content=\"Первый русскоязычный сайт для программистов-говноедов\"><meta name=\"keywords\" content=\"говно, говноед, govno, govnoed, gdisclaimer, gamerdisclaimer, debil, debilbot, \r\n        дебил, дебилбот\"><meta name=\"author\" content=\"gdisclaimer\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"manifest\" href=\"/static/favicons/manifest.webmanifest\"><link rel=\"icon\" type=\"image/x-icon\" href=\"/static/favicons/favicon.ico\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-16x16.png\" sizes=\"16x16\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-32x32.png\" sizes=\"32x32\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-192x192.png\" sizes=\"192x192\"><link rel=\"icon\" type=\"image/png\" href=\"/static/favicons/favicon-512x512.png\" sizes=\"512x512\"><link rel=\"apple-touch-icon\" href=\"/static/favicons/apple-touch-icon.png\"><meta property=\"og:title\" content=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.ResolveAttributeValue(description)
+		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.ResolveAttributeValue(title + " - govnoed.de")
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/layout/head.templ`, Line: 22, Col: 61}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/layout/head.templ`, Line: 21, Col: 67}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var2)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"><meta property=\"og:image\" content=\"/static/img/3.png\"><meta property=\"og:type\" content=\"website\"><link rel=\"stylesheet\" href=\"/static/css/main.css\"><link rel=\"stylesheet\" href=\"/static/css/splash.css\"><link rel=\"stylesheet\" href=\"/static/css/content_container.css\"><link rel=\"stylesheet\" href=\"/static/css/generate_key_style.css\"><title>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"><meta property=\"og:description\" content=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var3 string
-		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(title)
+		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.ResolveAttributeValue(description)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/layout/head.templ`, Line: 31, Col: 22}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/layout/head.templ`, Line: 22, Col: 61}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, " - govnoed.de</title></head>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\"><meta property=\"og:image\" content=\"/static/img/3.png\"><meta property=\"og:type\" content=\"website\"><link rel=\"stylesheet\" href=\"/static/css/main.css\"><link rel=\"stylesheet\" href=\"/static/css/splash.css\"><link rel=\"stylesheet\" href=\"/static/css/content_container.css\"><link rel=\"stylesheet\" href=\"/static/css/generate_key_style.css\"><title>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 string
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(title)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/layout/head.templ`, Line: 31, Col: 22}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, " - govnoed.de</title></head>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
